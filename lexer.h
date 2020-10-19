163942503478358e1394d38647e7daaffdbd6f48
micki <github@com-science.de>
2020-10-19 20:48:00 +0200

    Add function for adding patterns into lexer API

--- lexer.h
+++ lexer.h
@@ -15,12 +15,15 @@ typedef struct {
 // The return value is a token which must be provide to calls to other functions
 // like lexer() or lexer_finish().
 void *lexer_init(int fd, uint32_t timeout);
+
 // lexer() takes token from lexer_init and returns next token index or -1 for
 // timeout.
 int lexer(void *token);
+
 // lexer_finish() takes token from lexer_init() and shutsdown lexer internal
 // data structures.
 void lexer_finish(void *token);
-// lexer_addpattern() takes a pattern and puts it into the list of patterns
-// the lexer looks for.
-bool lexer_addpattern(char *pattern);
+
+// patterns_push_back() takes a pattern and puts it into the list of patterns
+// the lexer looks for. The lexer returns id if the pattern is found.
+void patterns_push_back(char *pattern, int id);
